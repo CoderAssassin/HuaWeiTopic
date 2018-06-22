e51539cb0c8ae330398471aa0948f54ebc2b0589
aliyang <aliyang@aliyang.com>
2018-06-22 15:49:49 +0800

    update

---
+++ src/T41.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+/**
+ * @Author:Aliyang
+ * @Data: Created in 下午3:33 18-6-22
+ * 学英语：学英语
+ * 思路：题目简单，就是麻烦，抄。递归可行
+ **/
+public class T41 {
+
+//    String[] nums=new String[]{"one","two","three","four","five","six","seven","eight","nine","ten"}
+//
+//    public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        while (sc.hasNext()){
+//
+//            long num=sc.nextLong();
+//
+//        }
+//
+//    }
+//
+//    private static String read(long num,StringBuilder sb){
+//
+//        long b=num%100000000/10000;//万
+//        long c=num%10000;
+//        long d=c/1000;//千
+//        long e=d/100;//百
+//        long f=d/10;//十
+//        long g=d%10;//个位
+//
+//
+//        if (b>0)
+//            read(b,sb);
+//        sb.append("million and ");
+//        if (d>0)
+//            read(d,sb);
+//        sb.append("thousand and ");
+//        if (e>0)
+//            read(e,sb);
+//        sb.append("hundred ");
+//        if (f>0){
+//
+//        }
+//
+//
+//    }
+}
