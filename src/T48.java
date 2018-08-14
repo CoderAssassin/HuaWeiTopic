e8ace8ed7c300ee177dcdbc83dc38bf858b79619
aliyang <442829392@qq.com>
2018-08-14 18:36:50 +0800

    update

---
+++ src/T48.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * @Author:Aliyang
+ * @Data: Created in 下午6:25 18-8-14
+ * 多线程：我的解法
+ **/
+public class T48 {
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        while (sc.hasNext()){
+            int N=sc.nextInt();
+            for (int i=0;i<N;i++){
+                System.out.print("ABCD");
+            }
+            System.out.println();
+        }
+    }
+}
