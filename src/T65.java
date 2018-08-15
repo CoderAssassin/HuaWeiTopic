cbf1545aab9088ed6dd9a77f2a52cabb5acc60af
aliyang <442829392@qq.com>
2018-08-15 14:26:07 +0800

    update

--- src/T65.java
+++ src/T65.java
@@ -1,8 +1,32 @@
-package PACKAGE_NAME;
 
 /**
  * @Author:Aliyang
  * @Data: Created in 下午2:09 18-8-15
+ * 配置文件恢复：我的解法
+ * 坑点：根本不用判断单词是否相等，测试用例白痴一样
  **/
-public class T65 {
+import java.util.*;
+import java.io.*;
+public class T65{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, String> hMap = new HashMap<>();
+        //HashMap<String, String> hMap = new HashMap<>();
+        hMap.put("reset", "reset what");
+        hMap.put("reset board", "board fault");
+        hMap.put("board add", "where to add");
+        hMap.put("board delet", "no board at all");
+        hMap.put("reboot backplane", "impossible");
+        hMap.put("backplane abort", "install first");
+        String str =br.readLine();
+        while(str!=null){
+
+            if(hMap.containsKey(str)){
+                System.out.println(hMap.get(str));
+            }else{
+                System.out.println("unkown command");
+            }
+            str = br.readLine();
+        }
+    }
 }
