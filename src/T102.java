290c8def99dae8afed154b3c12b21ecb63b3d269
aliyang <442829392@qq.com>
2018-08-17 14:46:24 +0800

    update

--- src/T102.java
+++ src/T102.java
@@ -1,8 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Date;
+import java.util.Scanner;
 
 /**
  * @Author:Aliyang
  * @Data: Created in 下午2:25 18-8-17
+ * Redraiment的走法：我的解法
  **/
 public class T102 {
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        while (sc.hasNext()){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            int[] dp=new int[n];//到第i个数的最大连续递增串长度
+            int res=0;
+            for (int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+                dp[i]=1;
+                for (int j=0;j<i;j++){
+                    if (arr[i]>arr[j])
+                        dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+                res=Math.max(res,dp[i]);
+            }
+            System.out.println(res);
+        }
+    }
 }
