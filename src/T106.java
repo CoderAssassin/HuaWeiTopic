90c6cffce0b5e69fab426b5ec15b8f2d6dc6d6c3
aliyang <442829392@qq.com>
2018-08-17 15:23:43 +0800

    update

--- src/T106.java
+++ src/T106.java
@@ -1,8 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 /**
  * @Author:Aliyang
  * @Data: Created in 下午3:09 18-8-17
+ * 求解立方根：别人解法
  **/
 public class T106 {
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        while (sc.hasNext()){
+            double num=sc.nextDouble();
+            System.out.format("%.1f",newton(num));
+            System.out.println();
+        }
+
+    }
+
+    private static double newton(double y){
+        double x;
+        for (x=1.0;Math.abs(x*x*x-y)>1e-7;x=(2*x+y/x/x)/3);
+        return x;
+    }
 }
